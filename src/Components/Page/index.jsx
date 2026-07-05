09e1ff575863644b3a1301ae3c48dab4996604fd
Jorge Matos <jisapjisap@gmail.com>
2026-07-05 20:32:38 +0200

    feat: add global stylesheet with Tailwind integration and implement responsive navbar and search modal components

--- src/Components/Page/index.jsx
+++ src/Components/Page/index.jsx
@@ -407,7 +407,7 @@ const Index = () => {
 
         <div className="container mx-auto max-w-7xl px-6 mt-12">
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-6 justify-items-center">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-6 justify-items-center">
 
             {cards.map(({ img, place }) => (
 
